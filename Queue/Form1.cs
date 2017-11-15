53a3d9433d0593eea99fd2dbbb24e6510460d9d0
Luigig8per <luisma@g8perhead.com>
2017-11-15 17:11:33 -0500

    Almost worrking with all data

--- Queue/Form1.cs
+++ Queue/Form1.cs
@@ -13,17 +13,27 @@ using BusinessObjects;
 using DataLayer;
 using System.Xml;
 using System.Xml.Linq;
-
+using System.Globalization;
+using Entities;
 
 
 namespace Queue
 {
     public partial class Form1 : Form
     {
+        line lineClass;
+        webReq theWebRequest = new webReq();
         public Form1()
         {
             InitializeComponent();
             receiver();
+        }
+
+        public async void doWebReq(string user, string pwd, string action, string rot, string sport, string period, string lineTypeID, string visitorML, string homeML, string total, string totalOver, string totalUnder, string visitorSpread, string visitorSpreadOdds, string homeSpread, string homeSpreadOdds, string draw, string sportBookId, string date)
+        {
+
+
+            await theWebRequest.asyncasyncmakePost(user, pwd, action, rot, sport, period, lineTypeID, visitorML, homeML, total, totalOver, totalUnder, visitorSpread, visitorSpreadOdds, homeSpread, homeSpreadOdds, draw, sportBookId, date);
         }
 
         private void receiver()
@@ -70,8 +80,62 @@ namespace Queue
 
         }
 
+        public object doQuery(string query)
+        {
+            Dbconnection dbCon = new Dbconnection();
+            return dbCon.ExeScalar(query);
+        }
+
+        DateTime convertToEastern(string originalDate)
+        {
+
+
+            int dateLength;
+            DateTime dt;
+
+
+            dateLength = originalDate.Length;
+
+
+
+            //'Actual received date is  "2017-11-10T13:02:37+0000"" />
+            //before was <heart_beat timestamp="17-11-09T15:30:39+0000" />
+            if (dateLength == 24)
+            {
+                dt = DateTime.ParseExact(originalDate, "yyyy-MM-dd'T'HH:mm:ssK",
+                                CultureInfo.InvariantCulture,
+                                DateTimeStyles.AdjustToUniversal);
+            }
+            else
+            {
+                dt = DateTime.ParseExact(originalDate, "yy-MM-dd'T'HH:mm:ssK",
+                               CultureInfo.InvariantCulture,
+                               DateTimeStyles.AdjustToUniversal);
+            }
+
+
+            TimeZoneInfo easternTimeZone = TimeZoneInfo.FindSystemTimeZoneById(
+                                 "Eastern Standard Time");
+
+            DateTime easternDateTime = TimeZoneInfo.ConvertTimeFromUtc(dt,
+                                                                       easternTimeZone);
+
+            return easternDateTime;
+        }
+
+
+        public object doQuery(string table, string column, string text)
+        {
+            string query;
+            query = "insert into " + table + "(" + column + ") values ('" + text + "')"; ;
+
+            Dbconnection dbCon = new Dbconnection();
+            return dbCon.ExeScalar(query);
+        }
+
         private void Message_Listener(IMessage message)
         {
+            Dictionary<string, string> dictionaryQuery = new Dictionary<string, string>();
             int cont = 0;
             string query="", query2="";
             string attrValue;
@@ -273,6 +337,138 @@ namespace Queue
             OperatorRequestObject OperatorRequestObject = ((BusinessObjects.OperatorRequestObject)(objMessage.Body));
 
            
+        }
+
+        public void postOng8Sports(line lineCls, Dictionary<string, string> dictionary)
+        {
+
+
+
+            if (lineCls.event_id != -9999)
+            {
+
+
+
+                string lineDate = this.getLineDate(lineCls.event_id).ToString();
+
+
+
+                if (lineDate != "NOID")
+                {
+
+                    lineDate = lineDate.Substring(0, 10);
+                    doWebReq("g8", "g8bridge", "setValue", lineCls.rot.ToString(), lineCls.sport_id.ToString(), lineCls.period_id.ToString(), "154", lineCls.ml_away_price.ToString(), lineCls.ml_home_price.ToString(), lineCls.total.ToString(), lineCls.over_price.ToString(), lineCls.under_price.ToString(), lineCls.ps_away_spread.ToString(), lineCls.ps_away_price.ToString(), lineCls.ps_home_spread.ToString(), lineCls.ps_home_price.ToString(), lineCls.draw_price.ToString(), lineCls.sportsbook.ToString(), lineDate);
+                }
+                else
+
+                {
+                    Console.WriteLine("There are not event id  " + lineCls.event_id + "in database, therefore can not be sent to API");
+                }
+            }
+        }
+
+        private string GetValue(string key, Dictionary<String, String> dictionary)
+        {
+            string returnValue;
+            if (!dictionary.TryGetValue(key, out returnValue))
+            {
+                returnValue = "-9999";
+            }
+            return returnValue;
+        }
+
+        public string getLineDate(int eventId)
+
+        {
+
+            string returnValue;
+
+            IDictionary<string, string> parametersDictionary = new Dictionary<string, string>();
+
+
+            parametersDictionary.Add("id_event", eventId.ToString());
+
+            Dbconnection theDbConnection = new Dbconnection();
+
+            DataTable theDatatable = new DataTable();
+
+            theDatatable = theDbConnection.ExeSPWithResults("get_date_from_event_id", parametersDictionary);
+
+            if (theDatatable.Rows.Count > 0)
+            {
+
+                returnValue = theDatatable.Rows[0][0].ToString();
+
+            }
+
+            else
+            {
+                returnValue = "NOID";
+
+            }
+
+            return returnValue;
+
+        }
+
+        public string getDonBestStatus()
+        {
+
+            IDictionary<string, string> parametersDictionary = new Dictionary<string, string>();
+
+            Dbconnection theDbConnection = new Dbconnection();
+
+            DataTable theDatatable = new DataTable();
+
+            theDatatable = theDbConnection.ExeSPWithResultsdb2("GetDonBestStatus", parametersDictionary);
+
+            string returnValue = theDatatable.Rows[0][0].ToString();
+
+
+
+            return returnValue;
+        }
+
+        public line createClassLine(Dictionary<String, String> dictionary)
+        {
+            //lineClass.away_price = int.Parse(dictionary["ps_away_money"]);
+            //lineClass.ps
+            //            user = "g8”
+            //pwd = “g8bridge”
+            //action = Request.Form("action")
+
+
+
+
+            lineClass.rot = int.Parse(GetValue("rot", dictionary));
+
+
+
+            //lineTypeID = 154
+
+
+            lineClass.sport_id = int.Parse(GetValue("sport_id", dictionary));
+            lineClass.away_money = int.Parse(GetValue("away_money", dictionary));
+            lineClass.period = GetValue("period", dictionary);
+            lineClass.period_id = int.Parse(GetValue("period_id", dictionary));
+            lineClass.ml_away_price = int.Parse(GetValue("ml_away_price", dictionary));
+            lineClass.ml_home_price = int.Parse(GetValue("ml_home_price", dictionary));
+            lineClass.total = float.Parse(GetValue("total", dictionary));
+            lineClass.over_price = int.Parse(GetValue("over_price", dictionary));
+            lineClass.under_price = int.Parse(GetValue("under_price", dictionary));
+            lineClass.ps_away_spread = float.Parse(GetValue("ps_away_spread", dictionary));
+            lineClass.ps_away_price = int.Parse(GetValue("ps_away_price", dictionary));
+            lineClass.ps_home_spread = float.Parse(GetValue("ps_home_spread", dictionary));
+            lineClass.ps_home_price = float.Parse(GetValue("ps_home_price", dictionary));
+            lineClass.draw_price = int.Parse(GetValue("draw_price", dictionary));
+            lineClass.ps_away_price = int.Parse(GetValue("ps_away_price", dictionary));
+            lineClass.sportsbook = short.Parse(GetValue("sportsbook", dictionary));
+            lineClass.event_id = int.Parse(GetValue("event_id", dictionary));
+
+            return lineClass;
+
+
+
         }
 
         public void extractXML(string xmlText)
---
+++ Queue/webReq.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Queue
+{
+    class webReq
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task asyncasyncmakePost(string user, string pwd, string action, string rot, string sport, string period, string lineTypeID, string visitorML, string homeML, string total, string totalOver, string totalUnder, string visitorSpread, string visitorSpreadOdds, string homeSpread, string homeSpreadOdds, string draw, string sportBookId, string date)
+
+        {
+            var values = new Dictionary<string, string>
+            {
+                  { "u", user },
+                 { "p", pwd },
+                  { "action", action },
+                  { "rot", rot },
+                   { "sid", sport },
+                   { "pid", period },
+                  { "lid", lineTypeID },
+                 { "vml", visitorML },
+                 { "hml", homeML },
+                 { "ttl", total },
+                  { "tov", totalOver },
+                  { "tun", totalUnder },
+                 { "vsd", visitorSpread },
+                  { "vso", visitorSpreadOdds },
+                   { "hsd", homeSpread },
+                   { "hso", homeSpreadOdds },
+                   { "draw", draw },
+                   { "sbkid", sportBookId },
+                { "gdt", date}
+
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("http://tools.golden8sports.com/bridge/donbest.asp", content);
+
+            if (response != null)
+            {
+                // Error Here
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseContent);
+            }
+
+            Console.WriteLine(" u :" + user + ", p :" + pwd + ", action: " + action + ",rot: " + rot + ",sid: " + sport + ",pid: " + period + ",lid: " + lineTypeID + ",vml: " + visitorML + ",hml: " + homeML + ",ttl: " + total + ",tov: " + totalOver + ",tun: " + totalUnder + ",vsd: " + visitorSpread + ",vso: " + visitorSpreadOdds + ",hsd: " + homeSpread + ",hso: " + homeSpreadOdds + ",draw: " + draw + ",sbkid: " + sportBookId + ",gdt: " + date + "");
+
+        }
+
+    }
+}
