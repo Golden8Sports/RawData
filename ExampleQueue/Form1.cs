6b6e2fd3498535e1ed3630e5b8aeecf550376dea
Luigig8per <luisma@g8perhead.com>
2017-11-15 14:00:34 -0500

    Working on localhost

---
+++ ExampleQueue/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apache.NMS;
+using Apache.NMS.Util;
+using BusinessObjects;
+
+
+namespace ExampleQueue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IObjectMessage objMessage;
+            OperatorRequestObject OperatorRequestObject = new OperatorRequestObject();
+            OperatorRequestObject.Shortcode = textBox1.Text.ToString();
+
+            IConnectionFactory factory = new NMSConnectionFactory("tcp://localhost:61616");
+            IConnection connection = factory.CreateConnection();
+            connection = factory.CreateConnection();
+            connection.Start();
+
+            ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            IDestination QueueDestination = SessionUtil.GetDestination(session, "ExampleQueue");
+            IMessageProducer MessageProducer = session.CreateProducer(QueueDestination);
+            objMessage = session.CreateObjectMessage(OperatorRequestObject);
+
+            MessageProducer.Send(objMessage);
+            session.Close();
+            connection.Stop();
+        }
+    }
+}
---
+++ Queue/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apache.NMS;
+using Apache.NMS.Util;
+using BusinessObjects;
+
+
+namespace Queue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            receiver();
+        }
+
+        private void receiver()
+        {
+            string topic = "com.donbest.message.public.xmleddie";
+            string userName;
+            string password;
+            string brokerUri;
+
+            topic = "com.donbest.message.public.xmleddie";
+            userName = "xmleddie";
+            password = "xmlfootball";
+            brokerUri = "tcp://amq.donbest.com:61616";
+           
+
+            IConnectionFactory factory = new NMSConnectionFactory("tcp://localhost:61616");
+            IConnection connection = factory.CreateConnection(userName,password);
+            connection.Start();
+            ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge);
+            IDestination destination = SessionUtil.GetDestination(session, "ExampleQueue");
+            IMessageConsumer receiver = session.CreateConsumer(destination);
+            receiver.Listener += new MessageListener(Message_Listener);
+
+        }
+
+        private void Message_Listener(IMessage message)
+        {
+            IObjectMessage objMessage = message as IObjectMessage;
+            OperatorRequestObject OperatorRequestObject = ((BusinessObjects.OperatorRequestObject)(objMessage.Body));
+            MessageBox.Show(OperatorRequestObject.Shortcode);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
